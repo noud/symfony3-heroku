6e99a45e33b785d551026c86f06a9082c8d79a1c
Noud de Brouwer <noud4@home.nl>
2019-07-03 01:22:54 +0200

    Heroku Log and Router, Symfony Doctrine DBAL and Monolog.

--- web/app.php
+++ web/app.php
@@ -16,6 +16,16 @@ if (PHP_VERSION_ID < 70000) {
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+
+// Trust Heroku Router
+Request::setTrustedProxies(
+    // trust *all* requests
+    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
+    
+    // only trust X-Forwarded-Port/-Proto, not -Host
+    Request::HEADER_X_FORWARDED_AWS_ELB
+);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
